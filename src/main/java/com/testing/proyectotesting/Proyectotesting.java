a6b5aad139281d0ee4f7103e7161ebcb2d68ed40
unknown <mhch101101@gmail.com>
2025-04-26 01:49:47 -0500

    primera subida

---
+++ src/main/java/com/testing/proyectotesting/Proyectotesting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.testing.proyectotesting;
+
+/**
+ *
+ * @author Moises_F16.7.24
+ */
+public class Proyectotesting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
