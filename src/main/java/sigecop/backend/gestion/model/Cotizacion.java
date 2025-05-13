f6baf687364ba3f89f4cbba6f2a41f703b6feee7
unknown <mhch101101@gmail.com>
2025-05-13 11:13:23 -0500

    correccion clases cotizacion

--- src/main/java/sigecop/backend/gestion/model/Cotizacion.java
+++ src/main/java/sigecop/backend/gestion/model/Cotizacion.java
@@ -2,21 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package sigecop.backend.gestion.model;
 
-package sigecop.backend.master.model;
-
-import sigecop.backend.utils.AuditBase;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.Date;
+import sigecop.backend.utils.AuditBase;
 
-import java.math.BigDecimal;
 /**
  *
- * @author jhochuq
+ * @author Moises_F16.7.24
  */
 @Data
 @Entity
@@ -24,20 +31,19 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "cotizacion", schema = "gestion")
-public class Cotizacion {
+public class Cotizacion extends AuditBase {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "estado_id", referencedColumnName = "id")
-    private EstadoCotizacion estadoCotizacion;
-    @Column(name = "monto",precision = 10, scale = 2)
+    private EstadoCotizacion estado;
+    @Column(name = "monto", precision = 10, scale = 2)
     private BigDecimal monto;
     @Column(name = "fecha_emision")
     private Date fechaEmision;
     @Column(name = "comentario")
     private String comentario;
-    
-    
 }
---
+++ src/main/java/sigecop/backend/gestion/model/EstadoCotizacion.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sigecop.backend.gestion.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sigecop.backend.utils.AuditBase;
+
+/**
+ *
+ * @author Moises_F16.7.24
+ */
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "estado_cotizacion", schema = "gestion")
+public class EstadoCotizacion extends AuditBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "descripcion")
+    private String descripcion;
+    @Column(name = "detalle")
+    private String detalle;
+    @Column(name = "inicial", nullable = false)
+    private Boolean inicial;
+}
