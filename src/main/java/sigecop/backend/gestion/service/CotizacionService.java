d7c40bd047869ee47680740332e9a8ea7fca6725
unknown <mhch101101@gmail.com>
2025-05-14 13:45:52 -0500

    correccion

--- src/main/java/sigecop/backend/gestion/service/CotizacionService.java
+++ src/main/java/sigecop/backend/gestion/service/CotizacionService.java
@@ -5,6 +5,7 @@
 package sigecop.backend.gestion.service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,8 +21,6 @@ import sigecop.backend.gestion.model.CotizacionProducto;
 import sigecop.backend.gestion.model.EstadoCotizacion;
 import sigecop.backend.gestion.model.EstadoSolicitud;
 import sigecop.backend.gestion.model.Solicitud;
-import sigecop.backend.gestion.model.SolicitudProducto;
-import sigecop.backend.gestion.model.SolicitudProveedor;
 import sigecop.backend.gestion.repository.CotizacionProductoRepository;
 import sigecop.backend.gestion.repository.CotizacionRepository;
 import sigecop.backend.gestion.repository.EstadoCotizacionRepository;
@@ -39,7 +38,7 @@ import sigecop.backend.utils.generic.ServiceGeneric;
  */
 @Service
 public class CotizacionService extends ServiceGeneric<CotizacionResponse, CotizacionRequest, Cotizacion> {
-
+    
     private final CotizacionRepository cotizacionRepository;
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -51,12 +50,12 @@ public class CotizacionService extends ServiceGeneric<CotizacionResponse, Cotiza
     private EstadoSolicitudRepository estadoSolicitudRepository;
     @Autowired
     private CotizacionProductoRepository cotizacionProductoRepository;
-
+    
     public CotizacionService(CotizacionRepository _cotizacionRepository) {
         super(CotizacionResponse.class, _cotizacionRepository);
         this.cotizacionRepository = _cotizacionRepository;
     }
-
+    
     @Override
     public List<Cotizacion> listBase(CotizacionRequest filter) {
         return cotizacionRepository.findByFilter(
@@ -65,7 +64,7 @@ public class CotizacionService extends ServiceGeneric<CotizacionResponse, Cotiza
                 filter.getEstadoId()
         );
     }
-
+    
     @Override
     public ObjectResponse<CotizacionResponse> postFind(CotizacionResponse response) {
         if (response != null && response.getId() != null) {
@@ -77,33 +76,33 @@ public class CotizacionService extends ServiceGeneric<CotizacionResponse, Cotiza
         }
         return new ObjectResponse<>(Boolean.TRUE, null, response);
     }
-
+    
     @Override
     public ObjectResponse<Cotizacion> recordToEntityEdit(Cotizacion entity, CotizacionRequest request) {
         //IMPLEMENTAR AQUI
         return new ObjectResponse<>(Boolean.TRUE, null, entity);
     }
-
+    
     @Override
     public ObjectResponse<Cotizacion> recordToEntityNew(CotizacionRequest request) {
         //IMPLEMENTAR AQUI
         return new ObjectResponse<>(Boolean.TRUE, null, new Cotizacion());
     }
-
+    
     public ObjectResponse aprobar(CotizacionRequest request) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Integer userId = (Integer) authentication.getPrincipal();
-
+        
         Optional<Cotizacion> optionalCotizacion = cotizacionRepository.findById(request.getId());
         if (optionalCotizacion.isEmpty()) {
             return new ObjectResponse<>(Boolean.FALSE, "No se encontró la cotización", null);
         }
-
+        
         Optional<Solicitud> optionalSolicitud = solicitudRepository.findById(optionalCotizacion.get().getSolicitudProveedor().getSolicitud().getId());
         if (optionalSolicitud.isEmpty()) {
             return new ObjectResponse<>(Boolean.FALSE, "No se encontró la solicitud de la cotización", null);
         }
-
+        
         Usuario usuario;
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(userId);
         if (optionalUsuario.isPresent()) {
@@ -115,7 +114,7 @@ public class CotizacionService extends ServiceGeneric<CotizacionResponse, Cotiza
                     null
             );
         }
-
+        
         EstadoCotizacion estadoCotizacion;
         Optional<EstadoCotizacion> optionalEstadoCotizacion = estadoCotizacionRepository.findById(Constantes.EstadoCotizacion.APROBADO);
         if (optionalEstadoCotizacion.isPresent()) {
@@ -138,29 +137,30 @@ public class CotizacionService extends ServiceGeneric<CotizacionResponse, Cotiza
                     null
             );
         }
-
+        
         Cotizacion cotizacion = optionalCotizacion.get();
         cotizacion.setEstado(estadoCotizacion);
         cotizacion.setUsuarioEstado(usuario);
         cotizacionRepository.save(cotizacion);
-
+        
         Solicitud solicitud = optionalSolicitud.get();
         solicitud.setEstado(estadoSolicitud);
+        solicitud.setFechaFinalizado(new Date());
         solicitud.setUsuarioEstado(usuario);
         solicitudRepository.save(solicitud);
-
+        
         return new ObjectResponse<>(Boolean.TRUE, null, null);
     }
-
+    
     public ObjectResponse archivar(CotizacionRequest request) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Integer userId = (Integer) authentication.getPrincipal();
-
+        
         Optional<Cotizacion> optionalCotizacion = cotizacionRepository.findById(request.getId());
         if (optionalCotizacion.isEmpty()) {
             return new ObjectResponse<>(Boolean.FALSE, "No se encontró la cotización", null);
         }
-
+        
         Usuario usuario;
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(userId);
         if (optionalUsuario.isPresent()) {
@@ -172,7 +172,7 @@ public class CotizacionService extends ServiceGeneric<CotizacionResponse, Cotiza
                     null
             );
         }
-
+        
         EstadoCotizacion estado;
         Optional<EstadoCotizacion> optionalEstado = estadoCotizacionRepository.findById(Constantes.EstadoCotizacion.ARCHIVADO);
         if (optionalEstado.isPresent()) {
@@ -184,12 +184,12 @@ public class CotizacionService extends ServiceGeneric<CotizacionResponse, Cotiza
                     null
             );
         }
-
+        
         Cotizacion cotizacion = optionalCotizacion.get();
         cotizacion.setEstado(estado);
         cotizacion.setUsuarioEstado(usuario);
         cotizacionRepository.save(cotizacion);
         return new ObjectResponse<>(Boolean.TRUE, null, null);
     }
-
+    
 }
